94bc0c41eed6914083bc2afd645bfbc54e41f1c9
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2022-01-22 17:50:21 +0300

    init commit

---
+++ handler/handler.go
@@ -0,0 +1,77 @@
+package handler
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func EncryptAES(key, text []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	b := base64.StdEncoding.EncodeToString(text)
+	ciphertext := make([]byte, aes.BlockSize+len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
+
+	return base64Encode(ciphertext), nil
+}
+
+func DecryptAES(key []byte, text string) ([]byte, error) {
+	bytes64, err := base64Decode(text)
+	if err != nil {
+		return nil, fmt.Errorf("файл не содержит данные в кодировке base64")
+	}
+
+	return decrypt(key, []byte(bytes64))
+}
+
+func decrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(text) < aes.BlockSize {
+		return nil, fmt.Errorf("шифр не подходит")
+	}
+
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	data, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func base64Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func base64Decode(str string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"file-encryptor/handler"
+	"flag"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+const appVersion string = "File-hasher;1.0;AES256\n"
+
+func readFileData(fileName string) (string, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при чтении файла - %v", fileName)
+	}
+	return string(bytes), nil
+}
+
+func rewriteFileData(fileName string, data []byte) error {
+	err := ioutil.WriteFile(fileName, data, fs.ModeAppend)
+	if err != nil {
+		return fmt.Errorf("ошибка записи закодированных данных в файл - %v", err)
+	}
+
+	return nil
+}
+
+func validateParameterData(path string, passphare string, encrypt bool, decrytp bool) error {
+	var text string = ""
+
+	if path == "" {
+		text += "- не указан путь к файлу, параметер - path\n"
+	}
+
+	if passphare == "" {
+		text += "- не указан пароль, параметер - passphare\n"
+	}
+
+	if encrypt && decrytp {
+		text += "- одновременно указаны флаги encrypt и decrytp (можно указать только один флаг)\n"
+	} else if !encrypt && !decrytp {
+		text += "- не указан ни один из флагов encrypt или decrytp (можно указать только один флаг)\n"
+	}
+
+	if text != "" {
+		return fmt.Errorf("ошибка:\n%v", text)
+	} else {
+		return nil
+	}
+}
+
+func main() {
+
+	path := flag.String("path", "", "путь к файлу")
+	passphare := flag.String("passphare", "", "пароль")
+	encrypt := flag.Bool("encrypt", false, "зашифровать файл (признак)")
+	decrypt := flag.Bool("decrypt", false, "расшифровать файл (признак)")
+
+	flag.Parse()
+
+	// Валидация параметров командной строки
+	err := validateParameterData(*path, *passphare, *encrypt, *decrypt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*key, err := handler.HashPassword(*passphare)
+	if err != nil {
+		log.Fatalf("ошибка, не удалось получить хеш от пароля: %v", err)
+	}*/
+
+	/*if handler.CheckPasswordHash(*passphare, key) {
+		log.Fatal("ошибка: указан не верный пароль", err)
+	}*/
+
+	// читаем данные файла в переменную
+	fData, err := readFileData(*path)
+	if err != nil {
+		log.Fatalf("ошибка: не удалось прочитать данные из файла: %v", err)
+	}
+
+	if *encrypt {
+		aesData, err := handler.EncryptAES([]byte(handler.GetMD5Hash(*passphare)), []byte(fData))
+		if err != nil {
+			log.Fatal("ошибка: не получилось зашифровать данные из файла - ", err)
+		}
+		rewriteFileData(*path, []byte(appVersion+aesData))
+	}
+
+	if *decrypt {
+		i := strings.Index(fData, "\n")
+		processedData := fData[i:]
+
+		data, err := handler.DecryptAES([]byte(handler.GetMD5Hash(*passphare)), processedData)
+
+		if err != nil {
+			log.Fatal("ошибка: не получилось расшифровать данные из файла - ", err)
+		}
+		rewriteFileData(*path, []byte(data))
+	}
+}
